ef7298331bf1151b3342373dcd550de5274afa42
ProfessorxT <tusharawte16@gmail.com>
2024-03-31 13:06:33 +0530

    first commit

---
+++ ESP8266_Witty_Cloud_Development_Board_to_WiFi/ESP8266_Witty_Cloud_Development_Board_to_WiFi.ino
@@ -0,0 +1,22 @@
+/*
+Statement   	:   To connect ESP8266 Witty Cloud Development Board to WiFi and print IP address of the n/w.
+*/
+
+#include <ESP8266WiFi.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  WiFi.begin("poco","Tushar@16raw"); // ssid and password
+  while(WiFi.status()!=WL_CONNECTED){ // Connect to wifi network
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("Wifi Connected");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
